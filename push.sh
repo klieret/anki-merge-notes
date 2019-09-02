0a901ca032a412622b5ce1c0c4e0d717d4b0f338
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-09-02 15:44:15 +0200

    [Ref] Rename folder

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cp merge_notes.py ~/.local/share/Anki2/addons
-cp -r merge_notes_files ~/.local/share/Anki2/addons
+cp -r merge_notes ~/.local/share/Anki2/addons
+cp -r merge_notes ~/.local/share/Anki2/addons21
