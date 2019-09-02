7146836fd75f4c07c5a7e7349dc0b03d5834bc39
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-09-02 15:45:03 +0200

    [Ref] Delte run.py in favor of __init__

--- merge_notes/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
-from anki.hooks import addHook
-from .merge_notes import MergeNotes
-
-mn = MergeNotes()
-addHook('browser.setupMenus', mn.setup_menu)
